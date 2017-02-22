cc98fb49c1fc5e2e041e9ae976208a6afe10a486
xuenanni <xuenanni@hotmail.com>
2017-02-22 10:50:40 -0500

    first

---
+++ xuenanni/exercise.js
@@ -0,0 +1,29 @@
+var exercise = {};
+
+exercise.salaries = null;
+exercise.salary = null;
+
+// load salary and data
+exercise.load = function(salary, boston){
+    exercise.salaries = boston.data;
+    exercise.salary = salary;
+};
+
+// get salaries larger than given salary
+exercise.findBiggerSalaries = function(){
+    return exercise.salaries.filter(exercise.largerSalary); 
+    
+
+};
+
+// filter function
+exercise.largerSalary = function largerSalary(item){
+
+    // ---------------------------------------------------
+    //   YOUR CODE
+    //
+    //     Return a boolean (true/false) if item
+    //     is larger than exercise.salary
+    // ---------------------------------------------------
+
+};
